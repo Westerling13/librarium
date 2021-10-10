e9123462236558331087df2e737e34348617b828
Alexander Munz <alex.munz@yandex.ru>
2021-10-10 22:00:29 +0300

    Added ProfileAdmin class

--- profiles/admin.py
+++ profiles/admin.py
@@ -4,6 +4,11 @@ from profiles.models import Profile
 from profiles.user import User
 
 
-@admin.register(User, Profile)
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """Административный класс для модели Пользователя"""
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
