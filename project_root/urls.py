df98e1179840f148b07463ed72b124184e49f430
Alexander Munz <a_munz@list.ru>
2022-03-15 21:29:00 +0300

    Added api prefix to books urls

--- project_root/urls.py
+++ project_root/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('books/', include('bookstore.urls')),
+    path('api/books/', include('bookstore.urls')),
     path('admin/', admin.site.urls),
 ]
