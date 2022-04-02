e05d3f04fb33993c395d3689dd741200d55b00ec
Alexander Munz <a_munz@list.ru>
2022-04-02 11:15:26 +0300

    Added BookDetailAPIView

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,9 +1,15 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 
 from bookstore.models import Book
-from bookstore.serializers import BookListCreateSerializer
+from bookstore.serializers import BookListCreateSerializer, BookSerializer
 
 
 class BookListCreateAPIView(ListCreateAPIView):
     serializer_class = BookListCreateSerializer
     queryset = Book.objects.all()
+
+
+class BookDetailAPIView(RetrieveAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    lookup_url_kwarg = 'book_id'
