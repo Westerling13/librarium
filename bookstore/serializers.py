e98fb0e34e159ab5ec7fbb7a32925b32d2405f40
Alexander Munz <a_munz@list.ru>
2022-04-02 21:34:06 +0300

    Added categories field to BookListCreateSerializer

--- bookstore/serializers.py
+++ bookstore/serializers.py
@@ -5,6 +5,12 @@ from rest_framework.exceptions import ValidationError
 from bookstore.models import Book, Author, Category
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title']
+
+
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
@@ -14,12 +20,13 @@ class AuthorSerializer(serializers.ModelSerializer):
 class BookListCreateSerializer(serializers.ModelSerializer):
     authors = AuthorSerializer(many=True)
     authors_ids = serializers.ListField(child=serializers.IntegerField(), write_only=True, allow_null=True)
+    categories = CategorySerializer(many=True)
 
     EMPTY_AUTHORS_VALIDATION_MESSAGE = 'У книги должны быть указаны авторы.'
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'dt_release', 'authors', 'authors_ids']
+        fields = ['id', 'title', 'dt_release', 'authors', 'authors_ids', 'categories']
 
     def validate(self, attrs):
         if not attrs['authors'] and not attrs['authors_ids']:
@@ -29,10 +36,14 @@ class BookListCreateSerializer(serializers.ModelSerializer):
 
     @atomic
     def create(self, validated_data):
+        categories = [Category.objects.create(**category) for category in validated_data.pop('categories')]
+        if not categories:
+            categories.append(Category.objects.get(title=Category.DEFAULT_TITLE))
         authors = [Author.objects.create(**author) for author in validated_data.pop('authors')]
         authors.extend(list(Author.objects.filter(id__in=validated_data.pop('authors_ids'))))
         book = super().create(validated_data)
         book.authors.add(*authors)
+        book.categories.add(*categories)
         return book
 
 
@@ -42,9 +53,3 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['title', 'authors']
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'title']
