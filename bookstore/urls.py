3ea9b28b780803adf72340e7e8d26247fa2a078b
Alexander Munz <a_munz@list.ru>
2022-04-02 11:15:42 +0300

    Added endpoint for BookDetailAPIView

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from bookstore.views import BookListCreateAPIView
+from bookstore.views import BookListCreateAPIView, BookDetailAPIView
 
 appname = 'bookstore'
 
 urlpatterns = [
     path('', BookListCreateAPIView.as_view(), name='list'),
+    path('<int:book_id>/', BookDetailAPIView.as_view(), name='detail'),
 ]
