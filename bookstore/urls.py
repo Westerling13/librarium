081f5684d5e8044e44add38abcd411624f4935b6
Alexander Munz <a_munz@list.ru>
2022-04-02 21:07:28 +0300

    Added endpoint for CategoryListCreateAPIView

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from bookstore.views import BookListCreateAPIView, BookDetailAPIView
+from bookstore.views import BookListCreateAPIView, BookDetailAPIView, CategoryListCreateAPIView
 
 appname = 'bookstore'
 
 urlpatterns = [
     path('books/', BookListCreateAPIView.as_view(), name='book_list'),
     path('books/<int:book_id>/', BookDetailAPIView.as_view(), name='book_detail'),
+    path('categories/', CategoryListCreateAPIView.as_view(), name='category_list'),
 ]
