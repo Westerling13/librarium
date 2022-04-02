50d4a2c0ca87fa5b9792948b8f975d56dbff375f
Alexander Munz <a_munz@list.ru>
2022-04-02 11:14:09 +0300

    Added AuthorSerializer

--- bookstore/serializers.py
+++ bookstore/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
-from bookstore.models import Book
+from bookstore.models import Book, Author
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['forename', 'surname', 'patronymic', 'short_name']
+        extra_kwargs = {
+            'forename': {'write_only': True},
+            'surname': {'write_only': True},
+            'patronymic': {'write_only': True},
+            'short_name': {'read_only': True},
+        }
 
 
 class BookListCreateSerializer(serializers.ModelSerializer):
