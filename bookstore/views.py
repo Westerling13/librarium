add7d04aec7f974d0f9208ebcba7e0b1303291cc
Alexander Munz <a_munz@list.ru>
2022-04-02 21:06:05 +0300

    Added CategoryListCreateAPIView

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 
-from bookstore.models import Book
-from bookstore.serializers import BookListCreateSerializer, BookSerializer
+from bookstore.models import Book, Category
+from bookstore.serializers import BookListCreateSerializer, BookSerializer, CategorySerializer
 
 
 class BookListCreateAPIView(ListCreateAPIView):
@@ -13,3 +13,8 @@ class BookDetailAPIView(RetrieveAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
     lookup_url_kwarg = 'book_id'
+
+
+class CategoryListCreateAPIView(ListCreateAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
