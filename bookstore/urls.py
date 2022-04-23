7009cb5028772df12bd3e9d175e62930ecac2897
Alexander Munz <a_munz@list.ru>
2022-04-23 15:31:16 +0300

    Removed categories endpoint and fixed book list endpoint

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from bookstore.views import BookListCreateAPIView, BookDetailAPIView, CategoryListCreateAPIView
+from bookstore.views import BookDetailAPIView, BookListAPIView
 
 appname = 'bookstore'
 
 urlpatterns = [
-    path('books/', BookListCreateAPIView.as_view(), name='book_list'),
+    path('books/', BookListAPIView.as_view(), name='book_list'),
     path('books/<int:book_id>/', BookDetailAPIView.as_view(), name='book_detail'),
-    path('categories/', CategoryListCreateAPIView.as_view(), name='category_list'),
 ]
