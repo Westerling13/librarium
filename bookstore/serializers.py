8eecb9e214989b3866a994f53be056d1ee4503b1
Alexander Munz <a_munz@list.ru>
2022-04-23 15:26:30 +0300

    Removed Category and BookListCreate serializers

--- bookstore/serializers.py
+++ bookstore/serializers.py
@@ -1,52 +1,12 @@
-from django.db.transaction import atomic
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
-from bookstore.models import Book, Author, Category
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'title']
+from bookstore.models import Book, Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['id', 'forename', 'surname', 'patronymic']
-
-
-class BookListCreateSerializer(serializers.ModelSerializer):
-    authors = AuthorSerializer(many=True)
-    authors_ids = serializers.ListField(child=serializers.IntegerField(), write_only=True, allow_null=True)
-    categories = CategorySerializer(many=True)
-    categories_ids = serializers.ListField(child=serializers.IntegerField(), write_only=True, allow_null=True)
-
-    EMPTY_AUTHORS_VALIDATION_MESSAGE = 'У книги должны быть указаны авторы.'
-
-    class Meta:
-        model = Book
-        fields = ['id', 'title', 'dt_release', 'authors', 'authors_ids', 'categories', 'categories_ids']
-
-    def validate(self, attrs):
-        if not attrs['authors'] and not attrs['authors_ids']:
-            raise ValidationError(self.EMPTY_AUTHORS_VALIDATION_MESSAGE)
-
-        return attrs
-
-    @atomic
-    def create(self, validated_data):
-        categories = [Category.objects.create(**category) for category in validated_data.pop('categories')]
-        categories.extend(list(Category.objects.filter(id__in=validated_data.pop('categories_ids'))))
-        if not categories:
-            categories.append(Category.objects.get(title=Category.DEFAULT_TITLE))
-        authors = [Author.objects.create(**author) for author in validated_data.pop('authors')]
-        authors.extend(list(Author.objects.filter(id__in=validated_data.pop('authors_ids'))))
-        book = super().create(validated_data)
-        book.authors.add(*authors)
-        book.categories.add(*categories)
-        return book
 
 
 class BookSerializer(serializers.ModelSerializer):
