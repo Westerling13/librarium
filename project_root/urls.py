a6e7a6c555cca4076c827a26307e4ff920814604
Alexander Munz <a_munz@list.ru>
2022-04-27 22:17:24 +0300

    Included users and profiles urls to main urls

--- project_root/urls.py
+++ project_root/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('api/bookstore/', include('bookstore.urls')),
+    path('api/users/', include('profiles.users_urls')),
+    path('api/profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
     path('api/swagger/', include('project_root.swagger.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
