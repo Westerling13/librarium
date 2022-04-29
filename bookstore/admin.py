3cc22decbae27e4074078638bf1aa3d0d35f8fdb
Alexander Munz <a_munz@list.ru>
2022-04-29 08:25:47 +0300

    Added BookReadingRecordAdmin class

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from bookstore.models import Author, Book, BookSection
+from bookstore.models import Author, Book, BookSection, BookReadingRecord
 
 
 @admin.register(Author)
@@ -16,3 +16,8 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookSection)
 class BookSectionAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(BookReadingRecord)
+class BookReadingRecordAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user', 'book')
