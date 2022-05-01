4c5da59567451ae0b4753ee4d00b8d5120c56f2c
Alexander Munz <a_munz@list.ru>
2022-05-01 21:52:58 +0300

    Added BookReadingRecordCreateAPIView

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,8 +1,8 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.generics import RetrieveAPIView, ListAPIView, CreateAPIView
 
 from bookstore.models import Book
-from bookstore.serializers import BookSerializer, BookDetailSerializer
+from bookstore.serializers import BookSerializer, BookDetailSerializer, BookReadingRecordSerializer
 
 
 class BookDetailAPIView(RetrieveAPIView):
@@ -19,3 +19,19 @@ class BookListAPIView(ListAPIView):
     def get(self, request, *args, **kwargs):
         """Список книг"""
         return self.list(request, *args, **kwargs)
+
+
+class BookReadingRecordCreateAPIView(CreateAPIView):
+    serializer_class = BookReadingRecordSerializer
+    queryset = Book.objects.all()
+    lookup_url_kwarg = 'book_id'
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['book'] = self.get_object()
+        return context
+
+    @extend_schema(summary='Взять книгу')
+    def post(self, request, *args, **kwargs):
+        """Взять книгу."""
+        return self.create(request, *args, **kwargs)
