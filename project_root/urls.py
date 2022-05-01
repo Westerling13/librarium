f4fcf1e954770557407a69f03d35a5987b7c6ab9
Alexander Munz <a_munz@list.ru>
2022-05-01 21:21:13 +0300

    Renamed auth app to auth_app

--- project_root/urls.py
+++ project_root/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/auth/', include('auth.urls')),
+    path('api/auth/', include('auth_app.urls')),
     path('api/bookstore/', include('bookstore.urls')),
     path('api/users/', include('profiles.users_urls')),
     path('api/profiles/', include('profiles.urls')),
