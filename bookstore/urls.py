afcebf2663856072b0e69da1a91721a515a49dd0
Alexander Munz <a_munz@list.ru>
2022-05-01 21:53:15 +0300

    Added endpoint for BookReadingRecordCreateAPIView

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from bookstore.views import BookDetailAPIView, BookListAPIView
+from bookstore.views import BookDetailAPIView, BookListAPIView, BookReadingRecordCreateAPIView
 
 appname = 'bookstore'
 
 urlpatterns = [
     path('books/', BookListAPIView.as_view(), name='book_list'),
     path('books/<int:book_id>/', BookDetailAPIView.as_view(), name='book_detail'),
+    path(
+        'books/<int:book_id>/reading_records/',
+        BookReadingRecordCreateAPIView.as_view(),
+        name='book_reading_record_create',
+    ),
 ]
