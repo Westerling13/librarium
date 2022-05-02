4b616107a6fd6f9a63331efa7d9ceccc7ccbebfb
Alexander Munz <a_munz@list.ru>
2022-05-02 12:32:29 +0300

    Added url path for library endpoints

--- project_root/urls.py
+++ project_root/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('api/auth/', include('auth_app.urls')),
     path('api/bookstore/', include('bookstore.urls')),
-    path('api/users/', include('profiles.users_urls')),
+    path('api/library/', include('profiles.library_urls')),
     path('api/profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
     path('api/swagger/', include('project_root.swagger.urls')),
