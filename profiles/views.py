a56d5a1d5bbe6c23e787f9276a5b68268633ac41
Alexander Munz <a_munz@list.ru>
2022-05-02 21:54:42 +0300

    Added get method to LibraryRecordListCreateAPIView

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,20 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView
 
 from profiles.models import LibraryRecord
 from profiles.serializers import LibraryRecordSerializer
 
 
-class LibraryRecordListCreateAPIView(CreateAPIView):
+class LibraryRecordListCreateAPIView(ListCreateAPIView):
     serializer_class = LibraryRecordSerializer
-    queryset = LibraryRecord.objects.all()
+
+    def get_queryset(self):
+        return LibraryRecord.objects.filter(user=self.request.user).select_related('book').order_by('dt_created')
+
+    @extend_schema(summary='Список книг пользователя')
+    def get(self, request, *args, **kwargs):
+        """По умолчанию возвращается список книг, находящихся в процессе прочтения."""
+        return self.list(request, *args, **kwargs)
 
     @extend_schema(summary='Взять книгу')
     def post(self, request, *args, **kwargs):
