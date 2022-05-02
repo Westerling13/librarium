3105b6ca6d91f0f63ab25a9e0c56886918159268
Alexander Munz <a_munz@list.ru>
2022-05-02 14:02:16 +0300

    Updated LibraryRecordSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,7 +1,12 @@
+from typing import Optional
+
 from django.db.transaction import atomic
 from rest_framework import serializers, exceptions
 from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
 
+from bookstore.models import Book
+from bookstore.serializers import BookDetailSerializer
 from profiles.models import LibraryRecord
 from profiles.user import User
 
@@ -36,32 +41,36 @@ class UserLoginSerializer(serializers.ModelSerializer):
 
 
 class LibraryRecordSerializer(serializers.ModelSerializer):
+    book_id = serializers.IntegerField(help_text='Идентификатор книги')
+
     class ValidationMessages:
         BOOK_IS_ALREADY_BEING_READ = 'Вы уже читаете эту книгу.'
         NO_FREE_BOOKS = 'Нет свободных экземпляров.'
 
     class Meta:
         model = LibraryRecord
-        fields = ['id', 'book', 'status']
+        fields = ['id', 'book_id', 'status']
         extra_kwargs = {
-            'book': {'write_only': True},
-            'status': {'read_only': True},
+            'book': {'read_only': True}, 'status': {'read_only': True}, 'book_id': {'write_only': True},
         }
 
     def validate(self, data: dict) -> dict:
         user = self.context['request'].user
-        if user.library_records.filter(book=data['book']).exists():
+        if user.library_records.filter(book_id=data['book_id'], status=LibraryRecord.READING).exists():
             raise exceptions.ValidationError(self.ValidationMessages.BOOK_IS_ALREADY_BEING_READ)
 
-        if not data['book'].free_copies_number:
+        book = get_object_or_404(Book, id=data['book_id'])
+        if not book.free_copies_number:
             raise exceptions.ValidationError(self.ValidationMessages.NO_FREE_BOOKS)
 
+        data['book'] = book
         data['user'] = user
         return data
 
     @atomic
     def create(self, validated_data: dict) -> LibraryRecord:
-        reading_record = super().create(validated_data)
-        validated_data['book'].free_copies_number -= 1
-        validated_data['book'].save(update_fields=['free_copies_number', 'dt_updated'])
-        return reading_record
+        book = validated_data.pop('book')
+        library_record = super().create(validated_data)
+        book.free_copies_number -= 1
+        book.save(update_fields=['free_copies_number', 'dt_updated'])
+        return library_record
