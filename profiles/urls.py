f5300427dca934e8d2aaa775534df2d922007c41
Alexander Munz <a_munz@list.ru>
2022-05-07 22:21:01 +0300

    Added endpoint for ProfileLibraryRecordDetailAPIView

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from profiles.views import ProfileLibraryRecordListAPIView
+from profiles.views import ProfileLibraryRecordListAPIView, ProfileLibraryRecordDetailAPIView
 
 app_name = 'profile'
 
 urlpatterns = [
     path('books/', ProfileLibraryRecordListAPIView.as_view(), name='profile_library_record_list'),
+    path('books/<int:book_id>/', ProfileLibraryRecordDetailAPIView.as_view(), name='profile_library_record_detail'),
 ]
