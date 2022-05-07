137e1d3bc19b3a400da3dca481af2cfa5cc8e9e9
Alexander Munz <a_munz@list.ru>
2022-05-07 22:20:49 +0300

    Added ProfileLibraryRecordDetailAPIView

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.db.models import QuerySet
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, GenericAPIView, get_object_or_404
+from rest_framework.mixins import UpdateModelMixin
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -25,3 +26,15 @@ class ProfileLibraryRecordListAPIView(ListCreateAPIView):
     def get(self, request: Request, *args, **kwargs) -> Response:
         """Список взятых книг."""
         return self.list(request, *args, **kwargs)
+
+
+class ProfileLibraryRecordDetailAPIView(GenericAPIView, UpdateModelMixin):
+    serializer_class = ProfileLibraryRecordSerializer
+    lookup_url_kwarg = 'book_id'
+    lookup_field = 'book_id'
+
+    def get_object(self):
+        return get_object_or_404(LibraryRecord, status=LibraryRecord.READING, book_id=self.kwargs['book_id'])
+
+    def post(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
