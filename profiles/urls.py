6d13fc747a68d9f4d528d3816a31ff3f18fe4bd0
Alexander Munz <a_munz@list.ru>
2022-05-07 21:59:38 +0300

    Added endpoint for ProfileLibraryRecordListAPIView

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,9 @@
-app_name = 'profiles'
+from django.urls import path
+
+from profiles.views import ProfileLibraryRecordListAPIView
+
+app_name = 'profile'
 
 urlpatterns = [
-
+    path('books/', ProfileLibraryRecordListAPIView.as_view(), name='profile_library_record_list'),
 ]
