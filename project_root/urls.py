ec3e675749e4740c1d6dde83d1f34fbb38c73b76
Alexander Munz <a_munz@list.ru>
2022-05-07 14:29:04 +0300

    Fixed profile urls in main urls module

--- project_root/urls.py
+++ project_root/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('api/auth/', include('auth_app.urls')),
     path('api/library/', include('library.urls')),
-    path('api/profiles/', include('profiles.urls')),
+    path('api/profile/', include('profiles.urls')),
     path('admin/', admin.site.urls),
     path('api/swagger/', include('project_root.swagger.urls')),
 ]
