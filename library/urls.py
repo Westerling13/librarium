b3783d764650e4bda65edcb36bb3c2a1d848116f
Alexander Munz <a_munz@list.ru>
2022-05-07 14:28:39 +0300

    Added endpoint for BookSectionListAPIView

--- library/urls.py
+++ library/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from library.views import BookDetailAPIView, BookListAPIView
+from library.views import BookDetailAPIView, BookListAPIView, BookSectionListAPIView
 
 appname = 'library'
 
 urlpatterns = [
     path('books/', BookListAPIView.as_view(), name='book_list'),
     path('books/<int:book_id>/', BookDetailAPIView.as_view(), name='book_detail'),
+    path('sections/', BookSectionListAPIView.as_view(), name='book_section_list'),
 ]
