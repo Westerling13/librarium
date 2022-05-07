366d076f41969e16282b559604a491130b285387
Alexander Munz <a_munz@list.ru>
2022-05-07 20:27:34 +0300

    Added types and filter_queryset to BookListAPIView

--- library/views.py
+++ library/views.py
@@ -1,6 +1,12 @@
+from typing import Type
+
+from django.db.models import QuerySet
 from drf_spectacular.utils import extend_schema
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.mixins import UpdateModelMixin
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
 
 from library.models import Book, BookSection
 from library.serializers import BookSerializer, BookDetailSerializer, BookOperateSerializer, BookSectionSerializer
@@ -10,14 +16,14 @@ class BookDetailAPIView(RetrieveAPIView, UpdateModelMixin):
     queryset = Book.objects.all()
     lookup_url_kwarg = 'book_id'
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> Type[ModelSerializer]:
         if self.request.method == 'GET':
             return BookDetailSerializer
 
         return BookOperateSerializer
 
     @extend_schema(summary="Взять книгу", request=None)
-    def post(self, request, *args, **kwargs):
+    def post(self, request: Request, *args, **kwargs) -> Response:
         """Взять книгу, если она еще не на руках и есть в наличии, иначе будет соответствующая ошибка."""
         return self.update(request, *args, **kwargs)
 
@@ -26,8 +32,15 @@ class BookListAPIView(ListAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all().order_by('section__number', 'title')
 
+    def filter_queryset(self, queryset: QuerySet) -> QuerySet:
+        section_key = self.request.query_params.get('section')
+        if not section_key:
+            return queryset
+
+        return queryset.filter(section__key=section_key)
+
     @extend_schema(summary='Список книг')
-    def get(self, request, *args, **kwargs):
+    def get(self, request: Request, *args, **kwargs) -> Response:
         """Список книг."""
         return self.list(request, *args, **kwargs)
 
@@ -37,6 +50,6 @@ class BookSectionListAPIView(ListAPIView):
     queryset = BookSection.objects.all()
 
     @extend_schema(summary='Список разделов')
-    def get(self, request, *args, **kwargs):
+    def get(self, request: Request, *args, **kwargs) -> Response:
         """Список книжных разделов."""
         return self.list(request, *args, **kwargs)
