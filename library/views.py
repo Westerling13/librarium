2ae5a5cf9575d27617444a61db86ba73788b2669
Alexander Munz <a_munz@list.ru>
2022-05-07 14:28:25 +0300

    Added post method to BookDetailAPIView and added BookSectionListAPIView

--- library/views.py
+++ library/views.py
@@ -1,14 +1,25 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.mixins import UpdateModelMixin
 
-from library.models import Book
-from library.serializers import BookSerializer, BookDetailSerializer
+from library.models import Book, BookSection
+from library.serializers import BookSerializer, BookDetailSerializer, BookOperateSerializer, BookSectionSerializer
 
 
-class BookDetailAPIView(RetrieveAPIView):
-    serializer_class = BookDetailSerializer
+class BookDetailAPIView(RetrieveAPIView, UpdateModelMixin):
     queryset = Book.objects.all()
     lookup_url_kwarg = 'book_id'
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return BookDetailSerializer
+
+        return BookOperateSerializer
+
+    @extend_schema(summary="Взять книгу", request=None)
+    def post(self, request, *args, **kwargs):
+        """Взять книгу, если она еще не на руках и есть в наличии, иначе будет соответствующая ошибка."""
+        return self.update(request, *args, **kwargs)
 
 
 class BookListAPIView(ListAPIView):
@@ -17,5 +28,15 @@ class BookListAPIView(ListAPIView):
 
     @extend_schema(summary='Список книг')
     def get(self, request, *args, **kwargs):
-        """Список книг"""
+        """Список книг."""
+        return self.list(request, *args, **kwargs)
+
+
+class BookSectionListAPIView(ListAPIView):
+    serializer_class = BookSectionSerializer
+    queryset = BookSection.objects.all()
+
+    @extend_schema(summary='Список разделов')
+    def get(self, request, *args, **kwargs):
+        """Список книжных разделов."""
         return self.list(request, *args, **kwargs)
