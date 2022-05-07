82e5b8d60f5360058068108d7613f37f2cb05194
Alexander Munz <a_munz@list.ru>
2022-05-07 21:58:40 +0300

    Added ProfileLibraryRecordSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,10 @@
+from typing import List
+
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
+from library.models import LibraryRecord
+from library.serializers import AuthorSerializer
 from profiles.user import User
 
 
@@ -31,3 +35,26 @@ class UserLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class ProfileLibraryRecordSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField(help_text='ID книги')
+    cover = serializers.SerializerMethodField(help_text='Обложка книги')
+    title = serializers.SerializerMethodField(help_text='Название книги')
+    authors = serializers.SerializerMethodField(help_text='Авторы книги')
+
+    class Meta:
+        model = LibraryRecord
+        fields = ['status', 'dt_return', 'id', 'cover', 'title', 'authors']
+
+    def get_id(self, library_record: LibraryRecord) -> int:
+        return library_record.book.id
+
+    def get_cover(self, library_record: LibraryRecord) -> str:
+        return library_record.book.cover.url if library_record.book.cover.name else ''
+
+    def get_title(self, library_record: LibraryRecord) -> str:
+        return library_record.book.title
+
+    def get_authors(self, library_record: LibraryRecord) -> List[dict]:
+        return AuthorSerializer(library_record.book.authors, many=True).data
